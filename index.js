63cd375b9339293c00951d1aaedeaa522255d85d
shchansky <gavrilenko71285@gmail.com>
2023-01-05 13:54:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const authRouter = require("./src/auth");
+const { getTokens } = require("./src/auth/utils");
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(authRouter);
+
+const port = 3001;
+app.listen(port, () => console.log(`Server listens port: ${port}`));
